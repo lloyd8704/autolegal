a3dbd56d90ad0961e10226f06d0a414e3a282954
ryanoc-14 <D@ggaW3ed2402>
2022-07-26 18:14:16 +0100

    html and javascript wip

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 document.getElementById("correspondenceButton").onclick = function () {
-    window.location.assign("localhost:8080/correspondence.html")
+    window.location.href="localhost:8080/correspondence.html"
 };
 
 document.getElementById("pleadingsButton").onclick = function () {
-    window.location.assign("localhost:8080/pleadings.html")
+    window.location.href="localhost:8080/pleadings.html"
 };
