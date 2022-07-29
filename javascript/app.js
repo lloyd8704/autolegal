74b2276649ec68ffb6a03cbe9499dfc3fc00dce9
ryanoc-14 <D@ggaW3ed2402>
2022-07-29 09:03:16 +0100

    delete to redo this

--- javascript/app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("correspondenceButton").onclick = function () {
-    window.location.href="correspondence.html"
-};
-
-document.getElementById("pleadingsButton").onclick = function () {
-    window.location.href="pleadings.html"
-};
