f16dc6b840cab7d1e78abce50e0bea702df34cd3
ryanoc-14 <D@ggaW3ed2402>
2022-07-29 19:38:14 +0100

    created new js files

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,2 +1,48 @@
-// continue here
-// video: 11min40seconds - https://www.youtube.com/watch?v=6BozpmSjk-Y
+import dashboard from "./views/dashboard.js";
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: dashboard },
+        // { path: "/correspondence", view: () => console.log("Viewing some correspondence") },
+        // { path: "/pleadings", view: () => console.log("Viewing some pleadings") }        
+    ];
+
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if(!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    }
+
+    const view = new match.route.view();
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+    
+    router();
+});
